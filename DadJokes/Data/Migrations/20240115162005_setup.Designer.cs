6f6e29dcf89a673da46e401d56059af9a9f1c8eb
Abel <abel97.ag@gmail.com>
2024-01-17 15:52:25 +0100

    Jokes tab
    Jokes tab added with ORM SQL database. Can create and manage jokes on site anonymously

--- DadJokes/Data/Migrations/20240115162005_setup.Designer.cs
+++ DadJokes/Data/Migrations/20240115162005_setup.Designer.cs
@@ -4,6 +4,7 @@ using DadJokes.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DadJokes.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240115162005_setup")]
+    partial class setup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -40,7 +42,7 @@ namespace DadJokes.Data.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Joke", (string)null);
+                    b.ToTable("Joke");
                 });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRole", b =>
